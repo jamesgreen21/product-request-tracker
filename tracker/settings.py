5f549dd8ddb60f6352bed34770e3cf0f1e190c0c
jamesgreen21 <jamesgreen210486@gmail.com>
2020-01-14 19:05:31 +0000

    Change to settings.py

--- tracker/settings.py
+++ tracker/settings.py
@@ -1,6 +1,6 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 IMAGE_UPLOADS = '/home/ubuntu/environment/tracker/static/uploads/img'
