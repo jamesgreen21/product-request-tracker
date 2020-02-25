3ebdc5b06d19d1834e1f64fff6228057d36144df
jamesgreen21 <44775216+jamesgreen21@users.noreply.github.com>
2020-02-25 17:18:35 +0000

    Update settings.py

--- tracker/settings.py
+++ tracker/settings.py
@@ -3,10 +3,3 @@ import os
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-IMAGE_UPLOADS = '/home/ubuntu/environment/tracker/static/uploads/img'
-ALLOWED_IMAGE_EXTENSIONS = ['PNG', 'JPG', 'JEPG']
-MAX_CONTENT_LENGTH = 16 * 1024 * 1024
-
-S3_BUCKET = os.environ.get('S3_BUCKET')
-S3_KEY = os.environ.get('S3_KEY')
-S3_SECRET = os.environ.get('S3_SECRET')
