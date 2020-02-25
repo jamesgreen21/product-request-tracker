1810f1f731f8aef61da4c008f32272d6905b3307
jamesgreen21 <44775216+jamesgreen21@users.noreply.github.com>
2020-02-25 17:25:06 +0000

    Update commands.py

--- tracker/commands.py
+++ tracker/commands.py
@@ -2,6 +2,7 @@ import click
 from flask.cli import with_appcontext
 
 from .extensions import db
+from .models import User, Suppliers, Posts, Actions
 
 @click.command(name='create_tables')
 @with_appcontext
